4853b0ab8fe67c12ea138f7fd617c2a601f38ac9
misrax <fordax05@gmail.com>
2025-03-21 23:06:18 +0300

    feat(db_template): Adding email, country to the User model
    Different template dirs for webiste and htmx

--- models/user.py
+++ models/user.py
@@ -10,6 +10,8 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     last_name = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=True)
+    country: Mapped[str] = mapped_column(String(50), nullable=True)
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, first_name={self.first_name!r}, last_name={self.last_name!r})"
--- views/website_view.py
+++ views/website_view.py
@@ -2,27 +2,27 @@ from flask import Blueprint, render_template
 
 from forms.contact_form import ContactForm
 
-website_blueprint = Blueprint('webiste', __name__, template_folder='templates')
+website_blueprint = Blueprint('website', __name__, template_folder='templates')
 
 
 @website_blueprint.route('/')
 def app_route_index():
-    return render_template('index.html')
+    return render_template('website/index.html')
 
 
 @website_blueprint.route('/about')
 def hello():
-    return render_template('about.html')
+    return render_template('website/about.html')
 
 
 @website_blueprint.route('/testimonial')
 def testimonial():
-    return render_template('testimonial.html')
+    return render_template('website/testimonial.html')
 
 
 @website_blueprint.route('/contact', methods=['GET', 'POST'])
 def contact():
     contact_form = ContactForm()
     if contact_form.validate_on_submit():
-        return render_template('contact-success.html', contact_form=contact_form)
-    return render_template('contact.html', contact_form=contact_form)
+        return render_template('website/contact-success.html', contact_form=contact_form)
+    return render_template('website/contact.html', contact_form=contact_form)
